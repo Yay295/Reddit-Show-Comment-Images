ccd1ad8000bae6996e39f05b2f2075838e03712c
Yay295 <Yay295@gmail.com>
2024-11-21 20:38:06 -0600

    set images to load lazily

--- Reddit Show Comment Images.user.js
+++ Reddit Show Comment Images.user.js
@@ -4,7 +4,7 @@
 // @namespace     https://github.com/Yay295/Reddit-Show-Comment-Images
 // @author        Yay295
 // @match         *://*.reddit.com/*
-// @version       1.0.0
+// @version       1.0.1
 // ==/UserScript==
 
 'use strict';
@@ -13,6 +13,7 @@ function processCommentImageLinks(img_links) {
 	for (let img_link of img_links) {
 		if (/^https:\/\/preview\.redd\.it\/[^\.]+\.[a-z]+/.test(img_link.href)) {
 			let img = document.createElement('img');
+			img.loading = 'lazy';
 			img.style.minWidth = '20px';
 			img.style.maxWidth = '240px';
 			img.style.minHeight = '20px';
